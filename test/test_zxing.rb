f30caa4916e7e416d64885981163d7499c599243
Alexandr Voinalovych <olexandr.voinalovych@gotoinc.co>
2025-02-27 16:54:44 +0200

    update rmagick

--- test/test_zxing.rb
+++ test/test_zxing.rb
@@ -1,58 +1,59 @@
-require File.expand_path( File.dirname(__FILE__) + '/test_helper')
+require 'minitest/autorun'
 require 'zxing'
 
-class ZXingTest < MiniTest::Test
-  context "A QR decoder singleton" do
+class ZXingTest < Minitest::Test
+  def setup
+    @decoder = ZXing
+    @uri = "http://2d-code.co.uk/images/bbc-logo-in-qr-code.gif"
+    @path = File.expand_path(File.dirname(__FILE__) + '/qrcode.png')
+    @file = File.new(@path)
+    @google_logo = "http://www.google.com/logos/grandparentsday10.gif"
+    @uri_result = "http://bbc.co.uk/programmes"
+    @path_result = "http://rubyflow.com"
+  end
 
-    class Foo < Struct.new(:v); def to_s; self.v; end; end
-
-    setup do
-      @decoder = ZXing
-      @uri = "http://2d-code.co.uk/images/bbc-logo-in-qr-code.gif"
-      @path = File.expand_path( File.dirname(__FILE__) + '/qrcode.png')
-      @file = File.new(@path)
-      @google_logo = "http://www.google.com/logos/grandparentsday10.gif"
-      @uri_result = "http://bbc.co.uk/programmes"
-      @path_result = "http://rubyflow.com"
-    end
-
-    should "decode a URL" do
-      assert_equal @decoder.decode(@uri), @uri_result
-    end
-
-    should "decode a file path" do
-      assert_equal @decoder.decode(@path), @path_result
-    end
-
-    should "return nil if #decode fails" do
-      assert_nil @decoder.decode(@google_logo)
-    end
-
-    should "raise an exception if #decode! fails" do
-      assert_raises(ZXing::ReaderException,
-                    ZXing::NotFoundException) { @decoder.decode!(@google_logo) }
-    end
-
-    should "decode objects that respond to #path" do
-      assert_equal @decoder.decode(@file), @path_result
-    end
-
-    should "call #to_s to argument passed in as a last resort" do
-      assert_equal @decoder.decode(Foo.new(@path)), @path_result
+  class Foo < Struct.new(:v)
+    def to_s
+      self.v
     end
   end
 
-  context "A QR decoder module" do
-    
-    setup do
-      class SpyRing; include ZXing end
-      @ring = SpyRing.new
-    end
+  def test_decode_url
+    assert_equal @uri_result, @decoder.decode(@uri)
+  end
 
-    should "include #decode and #decode! into classes" do
-      assert @ring.method(:decode)
-      assert @ring.method(:decode!)
-    end
+  def test_decode_file_path
+    assert_equal @path_result, @decoder.decode(@path)
+  end
 
+  def test_decode_returns_nil_on_failure
+    assert_nil @decoder.decode(@google_logo)
+  end
+
+  def test_decode_bang_raises_exception_on_failure
+    assert_raises(ZXing::ReaderException, ZXing::NotFoundException) do
+      @decoder.decode!(@google_logo)
+    end
+  end
+
+  def test_decode_objects_with_path_method
+    assert_equal @path_result, @decoder.decode(@file)
+  end
+
+  def test_decode_calls_to_s_as_last_resort
+    assert_equal @path_result, @decoder.decode(Foo.new(@path))
+  end
+end
+
+class ZXingModuleTest < Minitest::Test
+  def setup
+    @ring = Class.new do
+      include ZXing
+    end.new
+  end
+
+  def test_includes_decode_methods
+    assert @ring.respond_to?(:decode)
+    assert @ring.respond_to?(:decode!)
   end
 end
--- test/zxing/test_decodable.rb
+++ test/zxing/test_decodable.rb
@@ -1,38 +1,36 @@
-require File.expand_path( File.dirname(__FILE__) + '/../test_helper')
+require 'minitest/autorun'
 require 'zxing/decodable'
 
-class DecodableTest < MiniTest::Test
+class DecodableTest < Minitest::Test
+  # Extend File class with Decodable
+  File.include(Decodable)
 
-  class Object::File
-    include Decodable
-  end
-
+  # Test URL class that includes Decodable
   class URL
     include Decodable
     def initialize(path)
       @path = path
     end
-    def path; @path end
-  end
-
-  context "A Decodable module" do
-    setup do
-      @file = File.open( File.expand_path( File.dirname(__FILE__) + '/../qrcode.png' ))
-      @uri = URL.new "http://2d-code.co.uk/images/bbc-logo-in-qr-code.gif"
-      @bad_uri = URL.new "http://google.com"
-    end
-
-    should "provide #decode to decode the return value of #path" do
-      assert_equal @file.decode, ZXing.decode(@file.path)
-      assert_equal @uri.decode, ZXing.decode(@uri.path)
-      assert_nil @bad_uri.decode
-    end
-
-    should "provide #decode! as well" do
-      assert_equal @file.decode!, ZXing.decode(@file.path)
-      assert_equal @uri.decode!, ZXing.decode(@uri.path)
-      assert_raises(ZXing::BadImageException) { @bad_uri.decode! }
+    def path
+      @path
     end
   end
 
+  def setup
+    @file = File.open(File.expand_path(File.dirname(__FILE__) + '/../qrcode.png'))
+    @uri = URL.new("http://2d-code.co.uk/images/bbc-logo-in-qr-code.gif")
+    @bad_uri = URL.new("http://google.com")
+  end
+
+  def test_decode_provides_decoding_for_path_value
+    assert_equal ZXing.decode(@file.path), @file.decode
+    assert_equal ZXing.decode(@uri.path), @uri.decode
+    assert_nil @bad_uri.decode
+  end
+
+  def test_decode_bang_provides_decoding_with_exceptions
+    assert_equal ZXing.decode(@file.path), @file.decode!
+    assert_equal ZXing.decode(@uri.path), @uri.decode!
+    assert_raises(ZXing::BadImageException) { @bad_uri.decode! }
+  end
 end
